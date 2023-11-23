b31035cd6765d5eead13e64066cb7724fabf2854
skogseth <herman.skogseth@me.com>
2023-11-23 23:41:59 +0100

    Implement iterator, add some tests

--- src/stack.rs
+++ src/stack.rs
@@ -1,4 +1,5 @@
 use std::sync::atomic::{AtomicPtr, Ordering::*};
+use std::marker::PhantomData;
 
 pub struct Node<T> {
     val: T,
@@ -23,20 +24,88 @@ impl<T> SharedStack<T> {
     }
 
     pub fn push(&self, val: T) {
-        let top = &self.top;
         let node = Box::into_raw(Box::new(Node::new(val)));
         loop {
-            let old_top = top.load(Acquire);
+            let old_top = self.top.load(Acquire);
             unsafe { &*node }.next.store(old_top, Release);
-            if top.compare_exchange(old_top, node, SeqCst, SeqCst).is_ok() {
+            if self.top.compare_exchange(old_top, node, SeqCst, Relaxed).is_ok() {
                 break;
             }
         }
+    }
+
+    pub fn iter<'t>(&'t self) -> Iter<'t, T> {
+        let next = AtomicPtr::new(self.top.load(Relaxed));
+        Iter { next, _marker: PhantomData }
+    }
+
+    #[cfg(test)]
+    fn to_vec(&self) -> Vec<T> where T: Copy {
+        self.iter().copied().collect()
     }
 }
 
 impl<T> Default for SharedStack<T> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+pub struct Iter<'t, T> {
+    next: AtomicPtr<Node<T>>,
+    _marker: PhantomData<&'t SharedStack<T>>,
+}
+
+impl<'t, T> Iterator for Iter<'t, T> {
+    type Item = &'t T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = self.next.load(Acquire);
+        if next.is_null() {
+            return None;
+        }
+        let Node { val, next } = unsafe { &*next };
+        let new_next = next.load(Acquire);
+        self.next.store(new_next, Release);
+        Some(val)
+    }
+}
+
+impl<T> Drop for SharedStack<T> {
+    fn drop(&mut self) {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn stacks_first_test() {
+        let mut stack = SharedStack::new();
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        assert_eq!(stack.to_vec(), [2, 1, 0]);
+    }
+
+    #[test]
+    fn multiple_threads() {
+        let mut stack = SharedStack::new();
+
+        std::thread::scope(|s| {
+            s.spawn(|| {
+                stack.push(1);
+                stack.push(2);
+            }); 
+
+            s.spawn(|| {
+                stack.push(3);
+                stack.push(4);
+            });
+        });
+
+        assert_eq!(stack.to_vec().len(), 4);
     }
 }
