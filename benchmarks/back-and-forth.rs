2ab80add2bb4dc3de362a4bc4bda258c21a14532
Herman Skogseth <50301562+skogseth@users.noreply.github.com>
2024-01-28 12:13:25 +0100

    Add benchmarks (#17)

---
+++ benchmarks/back-and-forth.rs
@@ -0,0 +1,27 @@
+use hzrd::HzrdCell;
+
+const N: usize = 1_000_000;
+
+fn main() {
+    let cell = HzrdCell::new(None);
+
+    std::thread::scope(|s| {
+        s.spawn(|| {
+            for i in 0..N {
+                while cell.read().is_some() {
+                    std::hint::spin_loop();
+                }
+                cell.set(Some(i));
+            }
+        });
+
+        s.spawn(|| {
+            for _ in 0..N {
+                while cell.read().is_none() {
+                    std::hint::spin_loop();
+                }
+                cell.set(None);
+            }
+        });
+    });
+}
