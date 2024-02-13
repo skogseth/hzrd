8ddb2bf542797141d042df674c3148974e6cc2b9
skogseth <herman.skogseth@me.com>
2024-02-13 14:51:57 +0100

    Move remaining benchmarks

---
+++ benches/macro.rs
@@ -0,0 +1,78 @@
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use hzrd::{HzrdCell, LocalDomain};
+
+fn back_and_forth(n: usize) {
+    let cell = HzrdCell::new(None);
+
+    std::thread::scope(|s| {
+        s.spawn(|| {
+            for i in 0..n {
+                while cell.read().is_some() {
+                    std::hint::spin_loop();
+                }
+                cell.set(Some(i));
+            }
+        });
+
+        s.spawn(|| {
+            for _ in 0..n {
+                while cell.read().is_none() {
+                    std::hint::spin_loop();
+                }
+                cell.set(None);
+            }
+        });
+    });
+}
+
+fn local_writer(n: usize) {
+    use std::sync::Barrier;
+
+    let cell = HzrdCell::new_in(0, LocalDomain::new());
+    let barrier = Barrier::new(2);
+
+    std::thread::scope(|s| {
+        let mut reader = cell.reader();
+        let barrier = &barrier;
+        s.spawn(move || {
+            barrier.wait();
+            for _ in 0..n {
+                let _ = reader.get();
+            }
+        });
+
+        barrier.wait();
+        for i in 1..n {
+            cell.set(i);
+        }
+    });
+}
+
+fn single_threaded(n: usize) {
+    let cell = HzrdCell::new(0);
+
+    for i in 0..n {
+        let _handle = cell.get();
+        cell.set(i)
+    }
+}
+
+pub fn hzrd_cell(c: &mut Criterion) {
+    c.bench_function("back-and-forth", |b| {
+        b.iter(|| back_and_forth(black_box(1_000_000)))
+    });
+
+    c.bench_function("local-writer", |b| {
+        b.iter(|| local_writer(black_box(1_000_000)))
+    });
+
+    c.bench_function("single-threaded", |b| {
+        b.iter(|| single_threaded(black_box(1_000_000)))
+    });
+}
+
+criterion_group!(benches, hzrd_cell);
+criterion_main!(benches);
