7331ac4bcc2f6a6973903c64aab2b776c38e8673
skogseth <herman.skogseth@me.com>
2024-02-13 15:03:09 +0100

    Decrease number of samples

--- benches/macro.rs
+++ benches/macro.rs
@@ -74,5 +74,10 @@ pub fn hzrd_cell(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, hzrd_cell);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(20);
+    targets = hzrd_cell
+}
+
 criterion_main!(benches);
