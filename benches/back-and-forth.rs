eb0c9dd11dae3ac4d4196b624efe0220f8dd8d61
skogseth <herman.skogseth@me.com>
2024-02-13 14:42:52 +0100

    Move back-and-forth

--- benches/back-and-forth.rs
+++ benches/back-and-forth.rs
@@ -1,13 +1,15 @@
-use hzrd::HzrdCell;
+use std::hint::black_box;
 
-const N: usize = 1_000_000;
+use criterion::{criterion_group, criterion_main, Criterion};
 
-fn main() {
+fn back_and_forth(n: usize) {
+    use hzrd::HzrdCell;
+
     let cell = HzrdCell::new(None);
 
     std::thread::scope(|s| {
         s.spawn(|| {
-            for i in 0..N {
+            for i in 0..n {
                 while cell.read().is_some() {
                     std::hint::spin_loop();
                 }
@@ -16,7 +18,7 @@ fn main() {
         });
 
         s.spawn(|| {
-            for _ in 0..N {
+            for _ in 0..n {
                 while cell.read().is_none() {
                     std::hint::spin_loop();
                 }
@@ -25,3 +27,12 @@ fn main() {
         });
     });
 }
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("back-and-forth", |b| {
+        b.iter(|| back_and_forth(black_box(1_000_000)))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
